1e3db1c09c27b1ca328f6696792a2fcc80503342
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2023-05-09 23:16:45 +0200

    finished lesson 8 (ngFor directive for housing location list)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,20 +15,37 @@ import { HousingLocation } from '../housing-location';
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      >
+        ></app-housing-location
+      >
     </section>
   `,
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: 'assets/example-house.jpg',
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '/assets/example-house.jpg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    }
+  ];
 }
